b90c622993b39d0ed5dd31cd490fd7e3903aded8
Saif Aljanahi <saifalialjanahi@gmail.com>
2025-06-18 20:37:22 +0300

    fix: replace PrismJS component imports with manual language definitions to resolve extend() errors

--- src/utils/syntaxHighlighter.ts
+++ src/utils/syntaxHighlighter.ts
@@ -1,81 +1,305 @@
 import Prism from 'prismjs'
 import 'prismjs/themes/prism-tomorrow.css'
 
-// Manual initialization to avoid dependency conflicts
+// Manual initialization to avoid automatic loading conflicts
 Prism.manual = true
 
-// Core language definitions - load these first
+// Only import core components to avoid dependency conflicts
 import 'prismjs/components/prism-core'
-import 'prismjs/components/prism-clike'
-import 'prismjs/components/prism-markup'
-import 'prismjs/components/prism-css'
-import 'prismjs/components/prism-javascript'
 
-// Now load languages that depend on the core ones
-import 'prismjs/components/prism-json'
-import 'prismjs/components/prism-typescript'
-import 'prismjs/components/prism-jsx'
-import 'prismjs/components/prism-tsx'
+// Define all languages manually to avoid extend() dependency issues
+const defineLanguages = () => {
+  // JavaScript (base for many others)
+  Prism.languages.javascript = {
+    'comment': [
+      {
+        pattern: /(^|[^\\])\/\*[\s\S]*?(?:\*\/|$)/,
+        lookbehind: true,
+        greedy: true
+      },
+      {
+        pattern: /(^|[^\\:])\/\/.*/,
+        lookbehind: true,
+        greedy: true
+      }
+    ],
+    'string': [
+      {
+        pattern: /(["'])(?:(?!\1)[^\\\r\n]|\\(?:\r\n|[\s\S]))*\1/,
+        greedy: true
+      },
+      {
+        pattern: /`(?:[^`\\$]|\\[\s\S]|\$(?:\{(?:[^{}]|\{(?:[^{}]|\{[^}]*\})*\})*\}|(?!\{)))*`/,
+        greedy: true,
+        inside: {
+          'interpolation': {
+            pattern: /\$\{[^}]+\}/,
+            inside: {
+              'interpolation-punctuation': {
+                pattern: /^\$\{|\}$/,
+                alias: 'punctuation'
+              }
+            }
+          }
+        }
+      }
+    ],
+    'class-name': [
+      {
+        pattern: /(^|[^$\w\xA0-\uFFFF])(?!\s)[_$A-Z\xA0-\uFFFF](?:(?!\s)[$\w\xA0-\uFFFF])*(?=\.(?:prototype|constructor|[a-z])|[[(])/,
+        lookbehind: true
+      }
+    ],
+    'keyword': /\b(?:async|await|break|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|extends|finally|for|from|function|get|if|implements|import|in|instanceof|interface|let|new|null|of|package|private|protected|public|return|set|static|super|switch|this|throw|try|typeof|undefined|var|void|while|with|yield)\b/,
+    'boolean': /\b(?:false|true)\b/,
+    'function': /\w+(?=\()/,
+    'number': /\b0x[\da-f]+\b|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?/i,
+    'operator': /[<>]=?|[!=]=?=?|--?|\+\+?|&&?|\|\|?|[?*/~^%]/,
+    'punctuation': /[{}[\];(),.:]/
+  }
 
-// Load other languages
-import 'prismjs/components/prism-bash'
-import 'prismjs/components/prism-sql'
-import 'prismjs/components/prism-yaml'
-import 'prismjs/components/prism-markdown'
-import 'prismjs/components/prism-python'
-import 'prismjs/components/prism-java'
-import 'prismjs/components/prism-c'
-import 'prismjs/components/prism-cpp'
-import 'prismjs/components/prism-csharp'
-import 'prismjs/components/prism-php'
-import 'prismjs/components/prism-ruby'
-import 'prismjs/components/prism-swift'
-import 'prismjs/components/prism-kotlin'
-import 'prismjs/components/prism-scala'
-import 'prismjs/components/prism-dart'
-import 'prismjs/components/prism-rust'
-import 'prismjs/components/prism-go'
-import 'prismjs/components/prism-haskell'
-import 'prismjs/components/prism-ocaml'
-import 'prismjs/components/prism-scss'
-import 'prismjs/components/prism-less'
-import 'prismjs/components/prism-stylus'
-import 'prismjs/components/prism-xml-doc'
-import 'prismjs/components/prism-docker'
-import 'prismjs/components/prism-nginx'
-import 'prismjs/components/prism-apacheconf'
-import 'prismjs/components/prism-ini'
-import 'prismjs/components/prism-toml'
-import 'prismjs/components/prism-graphql'
-import 'prismjs/components/prism-regex'
+  // TypeScript
+  Prism.languages.typescript = {
+    ...Prism.languages.javascript,
+    'keyword': /\b(?:abstract|any|as|asserts|async|await|bigint|boolean|break|case|catch|class|const|continue|debugger|declare|default|delete|do|else|enum|export|extends|false|finally|for|from|function|get|if|implements|import|in|infer|instanceof|interface|is|keyof|let|module|namespace|never|new|null|number|object|of|package|private|protected|public|readonly|return|require|set|static|string|super|switch|symbol|this|throw|true|try|type|typeof|undefined|unique|unknown|var|void|while|with|yield)\b/,
+    'builtin': /\b(?:Array|ArrayBuffer|Boolean|DataView|Date|Error|EvalError|Float32Array|Float64Array|Function|Generator|GeneratorFunction|Int8Array|Int16Array|Int32Array|Intl|JSON|Map|Math|Number|Object|Promise|Proxy|RangeError|ReferenceError|Reflect|RegExp|Set|String|Symbol|SyntaxError|TypeError|Uint8Array|Uint8ClampedArray|Uint16Array|Uint32Array|URIError|WeakMap|WeakSet)\b/,
+    'class-name': {
+      pattern: /(\b(?:class|extends|implements|instanceof|interface|new|type)\s+)[\w.\\]+/,
+      lookbehind: true,
+      inside: {
+        'punctuation': /[.\\]/
+      }
+    }
+  }
+
+  // Go
+  Prism.languages.go = {
+    'comment': [
+      {
+        pattern: /(^|[^\\])\/\*[\s\S]*?(?:\*\/|$)/,
+        lookbehind: true,
+        greedy: true
+      },
+      {
+        pattern: /(^|[^\\:])\/\/.*/,
+        lookbehind: true,
+        greedy: true
+      }
+    ],
+    'string': {
+      pattern: /(^|[^\\])"(?:\\.|[^"\\])*"/,
+      lookbehind: true,
+      greedy: true
+    },
+    'keyword': /\b(?:break|case|chan|const|continue|default|defer|else|fallthrough|for|func|go|goto|if|import|interface|map|package|range|return|select|struct|switch|type|var)\b/,
+    'boolean': /\b(?:_|true|false|iota|nil)\b/,
+    'number': /(?:\b0x[a-f\d]+|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[-+]?\d+)?)i?/i,
+    'operator': /[*\/%^!=]=?|\+[=+]?|-[=-]?|\|[=|]?|&(?:=|&|\^=?)?|>(?:>=?|=)?|<(?:<=?|=|-)?|:=|\.\.\./,
+    'builtin': /\b(?:bool|byte|complex(?:64|128)|error|float(?:32|64)|rune|string|u?int(?:8|16|32|64)?|uintptr|append|cap|close|complex|copy|delete|imag|len|make|new|panic|print(?:ln)?|real|recover)\b/,
+    'punctuation': /[{}[\];(),.:]/
+  }
+
+  // JSON
+  Prism.languages.json = {
+    'property': {
+      pattern: /"(?:\\.|[^\\"\r\n])*"(?=\s*:)/,
+      greedy: true
+    },
+    'string': {
+      pattern: /"(?:\\.|[^\\"\r\n])*"(?!\s*:)/,
+      greedy: true
+    },
+    'comment': {
+      pattern: /\/\/.*|\/\*[\s\S]*?(?:\*\/|$)/,
+      greedy: true
+    },
+    'number': /-?\b\d+(?:\.\d+)?(?:e[+-]?\d+)?\b/i,
+    'punctuation': /[{}[\],]/,
+    'operator': /:/,
+    'boolean': /\b(?:false|true)\b/,
+    'null': {
+      pattern: /\bnull\b/,
+      alias: 'keyword'
+    }
+  }
+
+  // Python
+  Prism.languages.python = {
+    'comment': {
+      pattern: /(^|[^\\])#.*/,
+      lookbehind: true
+    },
+    'string-interpolation': {
+      pattern: /(?:f|rf|fr)(?:("""|''')[\s\S]*?\1|("|')(?:\\.|(?!\2)[^\\\r\n])*\2)/i,
+      greedy: true,
+      inside: {
+        'interpolation': {
+          pattern: /((?:^|[^{])(?:\{\{)*)\{(?:[^{}]|\{(?:[^{}]|\{[^}]*\})*\})*\}/,
+          lookbehind: true,
+          inside: {
+            'format-spec': {
+              pattern: /(:)[^:(){}]+(?=\}$)/,
+              lookbehind: true,
+              alias: 'number'
+            }
+          }
+        },
+        'string': /[\s\S]+/
+      }
+    },
+    'triple-quoted-string': {
+      pattern: /(?:[rub]|rb|br)?("""|''')[\s\S]*?\1/i,
+      greedy: true,
+      alias: 'string'
+    },
+    'string': {
+      pattern: /(?:[rub]|rb|br)?("|')(?:\\.|(?!\1)[^\\\r\n])*\1/i,
+      greedy: true
+    },
+    'function': {
+      pattern: /((?:^|\s)def[ \t]+)[a-zA-Z_]\w*(?=\s*\()/g,
+      lookbehind: true
+    },
+    'class-name': {
+      pattern: /(\bclass\s+)\w+/i,
+      lookbehind: true
+    },
+    'decorator': {
+      pattern: /(^[\t ]*)@\w+(?:\.\w+)*/im,
+      lookbehind: true,
+      alias: ['annotation', 'punctuation'],
+      inside: {
+        'punctuation': /\./
+      }
+    },
+    'keyword': /\b(?:and|as|assert|async|await|break|class|continue|def|del|elif|else|except|exec|finally|for|from|global|if|import|in|is|lambda|nonlocal|not|or|pass|print|raise|return|try|while|with|yield)\b/,
+    'builtin': /\b(?:__import__|abs|all|any|apply|ascii|basestring|bin|bool|buffer|bytearray|bytes|callable|chr|classmethod|cmp|coerce|compile|complex|delattr|dict|dir|divmod|enumerate|eval|execfile|file|filter|float|format|frozenset|getattr|globals|hasattr|hash|help|hex|id|input|int|intern|isinstance|issubclass|iter|len|list|locals|long|map|max|memoryview|min|next|object|oct|open|ord|pow|property|range|raw_input|reduce|reload|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|sum|super|tuple|type|unichr|unicode|vars|xrange|zip)\b/,
+    'boolean': /\b(?:False|None|True)\b/,
+    'number': /(?:\b(?=\d)|\B(?=\.))(?:0[bo])?(?:(?:\d|0x[\da-f])[\da-f]*(?:\.\d*)?|\.\d+)(?:e[+-]?\d+)?j?\b/i,
+    'operator': /[-+%=]=?|!=|\*\*?=?|\/\/?=?|<[<=>]?|>[=>]?|[&|^~]/,
+    'punctuation': /[{}[\];(),.:]/
+  }
+
+  // Bash
+  Prism.languages.bash = {
+    'shebang': {
+      pattern: /^#!\s*\/.*/,
+      alias: 'important'
+    },
+    'comment': {
+      pattern: /(^|[^"{\\$])#.*/,
+      lookbehind: true
+    },
+    'function-name': [
+      {
+        pattern: /(\bfunction\s+)\w+(?=(?:\s*\(?:\s*\))?\s*\{)/,
+        lookbehind: true,
+        alias: 'function'
+      },
+      {
+        pattern: /\b\w+(?=\s*\(\s*\)\s*\{)/,
+        alias: 'function'
+      }
+    ],
+    'for-or-select': {
+      pattern: /(\b(?:for|select)\s+)\w+(?=\s+in\s)/,
+      alias: 'variable',
+      lookbehind: true
+    },
+    'assign-left': {
+      pattern: /(^|[\s;|&]|[<>]\()\w+(?=\+?=)/,
+      inside: {
+        'environment': {
+          pattern: RegExp('(^|[\\s;|&]|[<>]\\()' + envVars),
+          lookbehind: true,
+          alias: 'constant'
+        }
+      },
+      alias: 'variable',
+      lookbehind: true
+    },
+    'string': [
+             {
+         pattern: /((?:^|[^<])<<-?\s*)(\w+?)\s[\s\S]*?(?:\r?\n|\r)\2/,
+         lookbehind: true,
+         greedy: true
+       },
+       {
+         pattern: /((?:^|[^<])<<-?\s*)(["'])(\w+)\2\s[\s\S]*?(?:\r?\n|\r)\3/,
+         lookbehind: true,
+         greedy: true
+       },
+       {
+         pattern: /(["'])(?:\\[\s\S]|\$\([^)]+\)|\$(?!\()|`[^`]+`|(?!\1)[^\\`$])*\1/,
+         greedy: true
+       }
+    ],
+    'environment': {
+      pattern: RegExp('\\$' + envVars),
+      alias: 'constant'
+    },
+    'variable': /\$(?:\w+|[#?*!@$])/,
+    'function': {
+      pattern: /(^|[\s;|&]|[<>]\()(?:add|apropos|apt-get|aptitude|aspell|awk|basename|bash|bc|bg|builtin|bzip2|cal|cat|cd|cfdisk|chgrp|chmod|chown|chroot|cksum|clear|cmp|comm|command|cp|cron|crontab|csplit|curl|cut|date|dc|dd|ddrescue|df|diff|diff3|dig|dir|dircolors|dirname|dirs|dmesg|du|egrep|eject|env|ethtool|eval|exec|expand|expect|export|expr|fdformat|fdisk|fg|fgrep|file|find|fmt|fold|format|free|fsck|ftp|fuser|gawk|getopts|git|grep|groupadd|groupdel|groupmod|groups|gzip|hash|head|help|hg|history|hostname|htop|iconv|id|ifconfig|ifdown|ifup|import|install|jobs|join|kill|killall|less|link|ln|locate|logname|ls|lsof|make|man|mkdir|mkfifo|mkisofs|mknod|more|most|mount|mtools|mtr|mv|mmv|nano|netstat|nice|nl|nohup|notify-send|nslookup|open|op|passwd|paste|pathchk|ping|pkill|popd|pr|printcap|printenv|printf|ps|pushd|pv|pwd|pydf|python|python2|python3|read|readarray|readonly|reboot|rename|renice|rev|rm|rmdir|rsync|screen|scp|sdiff|sed|seq|service|sftp|shift|shopt|shutdown|sleep|slocate|sort|source|split|ssh|stat|strace|su|sudo|sum|suspend|sync|tail|tar|tee|test|time|timeout|times|touch|top|traceroute|trap|tr|tsort|tty|type|ulimit|umask|umount|unalias|uname|unexpand|uniq|units|unset|unshar|uptime|useradd|userdel|usermod|users|uuencode|uudecode|v|vdir|vi|vim|virsh|vmstat|wait|watch|wc|wget|whereis|which|who|whoami|write|xargs|xdg-open|yes|zip)(?=$|[)\s;|&])/,
+      lookbehind: true
+    },
+    'keyword': {
+      pattern: /(^|[\s;|&]|[<>]\()(?:if|then|else|elif|fi|for|break|continue|while|until|case|esac|in|do|done|function|select|time|until)\b/,
+      lookbehind: true
+    },
+    'boolean': {
+      pattern: /(^|[\s;|&]|[<>]\()(?:true|false)(?=$|[)\s;|&])/,
+      lookbehind: true
+    },
+    'file-descriptor': {
+      pattern: /\B&\d\b/,
+      alias: 'important'
+    },
+    'operator': {
+      pattern: /\d?<>|>\||\+=?|==?|!=?|=~|<<[<-]?|[&\d]?>>|\d?[<>]&?|&[>&]?|\|[&|]?|<=?|>=?/,
+      inside: {
+        'file-descriptor': {
+          pattern: /^\d/,
+          alias: 'important'
+        }
+      }
+    },
+    'punctuation': /\$?\(\(?|\)\)?|\.\.|[{}[\];\\]/,
+    'number': {
+      pattern: /(^|\s)(?:[1-9]\d*|0)(?:[.,]\d+)?\b/,
+      lookbehind: true
+    }
+  }
+
+  // Basic fallback for unsupported languages
+  Prism.languages.text = {}
+  Prism.languages.plain = {}
+
+  // Add aliases
+  Prism.languages.js = Prism.languages.javascript
+  Prism.languages.ts = Prism.languages.typescript
+  Prism.languages.py = Prism.languages.python
+  Prism.languages.shell = Prism.languages.bash
+  Prism.languages.sh = Prism.languages.bash
+}
+
+// Environment variables pattern for bash
+const envVars = '(?:BASH|BASHOPTS|BASH_ALIASES|BASH_ARGC|BASH_ARGV|BASH_CMDS|BASH_COMPLETION_COMPAT_DIR|BASH_LINENO|BASH_REMATCH|BASH_SOURCE|BASH_SUBSHELL|BASH_VERSINFO|BASH_VERSION|COLORTERM|COLUMNS|COMP_WORDBREAKS|DBUS_SESSION_BUS_ADDRESS|DEFAULTS_PATH|DESKTOP_SESSION|DIRSTACK|DISPLAY|EUID|GDMSESSION|GDM_LANG|GNOME_KEYRING_CONTROL|GNOME_KEYRING_PID|GPG_AGENT_INFO|GROUPS|HISTCONTROL|HISTFILE|HISTFILESIZE|HISTSIZE|HOME|HOSTNAME|HOSTTYPE|IFS|INSTANCE|JOB|LANG|LANGUAGE|LC_ADDRESS|LC_ALL|LC_IDENTIFICATION|LC_MEASUREMENT|LC_MONETARY|LC_NAME|LC_NUMERIC|LC_PAPER|LC_TELEPHONE|LC_TIME|LESSCLOSE|LESSOPEN|LINES|LOGNAME|LS_COLORS|MACHTYPE|MAILCHECK|MANDATORY_PATH|NO_AT_BRIDGE|OLDPWD|OPTERR|OPTIND|ORBIT_SOCKETDIR|OSTYPE|PAPERSIZE|PATH|PIPESTATUS|PPID|PS1|PS2|PS3|PS4|PWD|RANDOM|REPLY|SECONDS|SELINUX_INIT|SESSION|SESSIONTYPE|SESSION_MANAGER|SHELL|SHELLOPTS|SHLVL|SSH_AUTH_SOCK|TERM|UID|UPSTART_EVENTS|UPSTART_INSTANCE|UPSTART_JOB|UPSTART_SESSION|USER|WINDOWID|XAUTHORITY|XDG_CONFIG_DIRS|XDG_CURRENT_DESKTOP|XDG_DATA_DIRS|XDG_GREETER_DATA_DIR|XDG_MENU_PREFIX|XDG_RUNTIME_DIR|XDG_SEAT|XDG_SEAT_PATH|XDG_SESSION_DESKTOP|XDG_SESSION_ID|XDG_SESSION_PATH|XDG_SESSION_TYPE|XDG_VTNR|XMODIFIERS)'
+
+// Initialize languages after a short delay
+setTimeout(defineLanguages, 50)
 
 // Comprehensive language mapping for common aliases
 const languageMap: Record<string, string> = {
   'ts': 'typescript',
   'js': 'javascript',
-  'jsx': 'jsx',
-  'tsx': 'tsx',
-  'rs': 'rust',
-  'hs': 'haskell',
-  'ml': 'ocaml',
+  'py': 'python',
   'sh': 'bash',
   'shell': 'bash',
-  'yml': 'yaml',
-  'md': 'markdown',
-  'py': 'python',
-  'rb': 'ruby',
-  'cs': 'csharp',
-  'cpp': 'cpp',
-  'c++': 'cpp',
-  'kt': 'kotlin',
-  'dockerfile': 'docker',
-  'conf': 'nginx',
-  'config': 'ini',
-  'gql': 'graphql',
+  'golang': 'go',
   'html': 'markup',
   'xml': 'markup',
-  'svg': 'markup',
-  'golang': 'go'
+  'svg': 'markup'
 }
 
 // Function to safely check if a language is available
@@ -88,51 +312,6 @@ const isLanguageAvailable = (language: string): boolean => {
     return false
   }
 }
-
-// Create fallback language definitions for common cases
-const ensureFallbackLanguages = () => {
-  // Ensure Go language is available
-  if (!isLanguageAvailable('go')) {
-    try {
-      Prism.languages.go = {
-        'comment': [
-          {
-            pattern: /(^|[^\\])\/\*[\s\S]*?(?:\*\/|$)/,
-            lookbehind: true,
-            greedy: true
-          },
-          {
-            pattern: /(^|[^\\:])\/\/.*/,
-            lookbehind: true,
-            greedy: true
-          }
-        ],
-        'string': {
-          pattern: /(^|[^\\])"(?:\\.|[^"\\])*"/,
-          lookbehind: true,
-          greedy: true
-        },
-        'keyword': /\b(?:break|case|chan|const|continue|default|defer|else|fallthrough|for|func|go|goto|if|import|interface|map|package|range|return|select|struct|switch|type|var)\b/,
-        'boolean': /\b(?:_|true|false|iota|nil)\b/,
-        'number': /(?:\b0x[a-f\d]+|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[-+]?\d+)?)i?/i,
-        'operator': /[*\/%^!=]=?|\+[=+]?|-[=-]?|\|[=|]?|&(?:=|&|\^=?)?|>(?:>=?|=)?|<(?:<=?|=|-)?|:=|\.\.\./,
-        'builtin': /\b(?:bool|byte|complex(?:64|128)|error|float(?:32|64)|rune|string|u?int(?:8|16|32|64)?|uintptr|append|cap|close|complex|copy|delete|imag|len|make|new|panic|print(?:ln)?|real|recover)\b/,
-        'punctuation': /[{}[\];(),.:]/
-      }
-      console.log('Fallback Go language definition created')
-    } catch (error) {
-      console.warn('Failed to create fallback Go language definition:', error)
-    }
-  }
-
-  // Ensure text/plain language is available as ultimate fallback
-  if (!isLanguageAvailable('text')) {
-    Prism.languages.text = {}
-  }
-}
-
-// Initialize fallback languages after a short delay to ensure all imports are loaded
-setTimeout(ensureFallbackLanguages, 100)
 
 // Function to safely highlight code with robust error handling
 export const highlightCode = (code: string, language: string = ''): string => {
@@ -173,40 +352,10 @@ export const getLanguageLabel = (language: string): string => {
   const labels: Record<string, string> = {
     'typescript': 'TypeScript',
     'javascript': 'JavaScript',
-    'jsx': 'React JSX',
-    'tsx': 'React TSX',
-    'rust': 'Rust',
     'go': 'Go',
-    'haskell': 'Haskell',
-    'ocaml': 'OCaml',
     'json': 'JSON',
     'bash': 'Bash',
-    'sql': 'SQL',
-    'yaml': 'YAML',
-    'markdown': 'Markdown',
-    'css': 'CSS',
-    'scss': 'SCSS',
-    'less': 'Less',
-    'stylus': 'Stylus',
-    'markup': 'HTML',
     'python': 'Python',
-    'java': 'Java',
-    'c': 'C',
-    'cpp': 'C++',
-    'csharp': 'C#',
-    'php': 'PHP',
-    'ruby': 'Ruby',
-    'swift': 'Swift',
-    'kotlin': 'Kotlin',
-    'scala': 'Scala',
-    'dart': 'Dart',
-    'docker': 'Docker',
-    'nginx': 'Nginx',
-    'apache': 'Apache',
-    'ini': 'Configuration',
-    'toml': 'TOML',
-    'graphql': 'GraphQL',
-    'regex': 'Regular Expression',
     'text': 'Plain Text'
   }
   
