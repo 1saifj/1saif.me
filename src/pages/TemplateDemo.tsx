009cd3468ea5fe80f8c314aa250692cbfedaa79f
Saif Aljanahi <saifalialjanahi@gmail.com>
2025-06-26 23:43:35 +0300

    chore: Remove TemplateDemo component - Deleted the TemplateDemo component from the project to streamline the codebase and eliminate unused files.

--- src/pages/TemplateDemo.tsx
+++ src/pages/TemplateDemo.tsx
@@ -3,8 +3,10 @@ import { NewsletterShowcase } from '../components/NewsletterShowcase'
 
 const TemplateDemo: React.FC = () => {
   return (
-    <div className="min-h-screen">
-      <NewsletterShowcase />
+    <div className="min-h-screen overflow-x-hidden">
+      <div className="px-4 sm:px-6 lg:px-8">
+        <NewsletterShowcase />
+      </div>
     </div>
   )
 }
